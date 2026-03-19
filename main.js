48be080c393d739d471b1d53099bf95d12459542
yardas-dev <selector.yardas_0u@icloud.com>
2026-03-19 12:20:47 +0100

    Inicializa el juego de la adivinanza

---
+++ main.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser'
+import scene from './scene.js'
+
+const WIDTH = 1920
+const HEIGHT = 1080
+
+class Juego {
+    constructor () {
+        this.config = {
+            type: Phaser.AUTO,
+            width: WIDTH,
+            height: HEIGHT,
+            scene,
+        }
+
+        this.juego = new Phaser.game(this.config)
+
+        this.reiniciar()
+    }
+
+    comprobar (n) {
+        if ( this.adivinanza === n ) {
+            console.log('¡Asertaste a peseta!')
+        } else if ( this.estáCerca(n) ) {
+            console.log('¡Caliente!')
+        } else {
+            console.log('Frío…')
+        }
+    }
+
+    estáCerca (n) {
+        if ( this.adivinanza < n && this.adivinanza + 10 >= n ) {
+            return true
+        } else if ( this.adivinanza > n && this.adivinanza - 10 <= n ) {
+            return true
+        }
+
+        return false
+    }
+
+    reiniciar () {
+        this.adivinanza = Math.floor(Math.random() * 100) + 1
+        console.log('Adivina el número en que pienso, del 1 al 100. ¡Vamos!')
+    }
+}
+
+export default Juego
