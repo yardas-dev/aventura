f746e69d889b6b2f577d823552a73fca1a6c25aa
yardas-dev <selector.yardas_0u@icloud.com>
2026-03-19 13:59:20 +0100

    Reestructura los métodos para darle el prefijo de privado

--- main.js
+++ main.js
@@ -15,20 +15,22 @@ class Juego {
 
         this.juego = new Phaser.game(this.config)
 
-        this.reiniciar()
+        this.#reiniciar()
     }
 
     comprobar (n) {
         if ( this.adivinanza === n ) {
-            console.log('¡Asertaste a peseta!')
-        } else if ( this.estáCerca(n) ) {
+            confirm('¡Asertaste a peseta! ¿Quieres volver a jugar?')
+                ? this.#reiniciar()
+                : undefined
+        } else if ( this.#estáCerca(n) ) {
             console.log('¡Caliente!')
         } else {
             console.log('Frío…')
         }
     }
 
-    estáCerca (n) {
+    #estáCerca (n) {
         if ( this.adivinanza < n && this.adivinanza + 10 >= n ) {
             return true
         } else if ( this.adivinanza > n && this.adivinanza - 10 <= n ) {
@@ -38,7 +40,7 @@ class Juego {
         return false
     }
 
-    reiniciar () {
+    #reiniciar () {
         this.adivinanza = Math.floor(Math.random() * 100) + 1
         console.log('Adivina el número en que pienso, del 1 al 100. ¡Vamos!')
     }
