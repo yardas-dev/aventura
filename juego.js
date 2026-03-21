ac8e6586f9b0483b32397c77ab3c8513e2e0afef
yardas-dev <selector.yardas_0u@icloud.com>
2026-03-21 17:07:54 +0100

    Refactoriza todo el juego y lo renombra como juego.js

---
+++ juego.js
@@ -0,0 +1,36 @@
+class Juego {
+    pistas = {
+        VICTORIA: 0,
+        COMPARACIÓN: 1,
+        NIVEL_PROXIMIDAD: 2
+    }
+    pista = undefined
+    intentos = 0
+    #adivinanza = undefined
+
+    constructor (pista = this.pistas.COMPARACIÓN, n = 1, m = 100) {
+        this.pista = pista
+        this.#adivinanza = Math.floor(Math.random() * n) + m
+    }
+
+    adivinar (n) {
+        if ( this.adivinanza === n ) {
+            return {
+                pista: this.pistas.VICTORIA,
+                resultado: true
+            }
+        } else if ( this.pista === this.pistas.COMPARACIÓN ) {
+            return {
+                pista: this.pistas.COMPARACIÓN,
+                resultado: this.#esMayor(n)
+            }
+        } else if ( this.pista === this.pistas.NIVEL_PROXIMIDAD ) {
+            return {
+                pista: this.pistas.NIVEL_PROXIMIDAD,
+                resultado: this.#obtenerNivelProximidad(n)
+            }
+        }
+    }
+}
+
+export default Juego
