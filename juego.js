0f2b8ba9a43773b1098ad121f0f9cd52684a0c41
yardas-dev <selector.yardas_0u@icloud.com>
2026-03-21 20:43:33 +0100

    Refactoriza la clase Juego y actualiza la lógica de adivinar

--- juego.js
+++ juego.js
@@ -1,37 +1,44 @@
 class Juego {
-    pistas = {
-        VICTORIA: 0,
-        COMPARACIÓN: 1,
-        NIVEL_PROXIMIDAD: 2
-    }
-    pista = undefined
-    intentos = 0
-    #adivinanza = undefined
+    AFIRMACIÓN = 'afirmación'
+    COMPARACIÓN = 'comparación'
+    NIVEL_PROXIMIDAD = 'nivel de proximidad (decimal)'
 
-    constructor (pista = this.pistas.COMPARACIÓN, n = 1, m = 100) {
-        this.pista = pista
+    #adivinanza = undefined
+    #hasAcertado = false
+    #intentos = 0
+    #tipoPista = undefined
+
+    constructor (tipoResultado = this.COMPARACIÓN, n = 1, m = 100) {
         this.#adivinanza = Math.floor(Math.random() * n) + m
+        this.#tipoResultado = tipoResultado
     }
 
     adivinar (n) {
-        this.intentos++
-
-        if ( this.adivinanza === n ) {
-            return {
-                pista: this.pistas.VICTORIA,
-                resultado: true
-            }
-        } else if ( this.pista === this.pistas.COMPARACIÓN ) {
-            return {
-                pista: this.pistas.COMPARACIÓN,
-                resultado: this.#esMayor(n)
-            }
-        } else if ( this.pista === this.pistas.NIVEL_PROXIMIDAD ) {
-            return {
-                pista: this.pistas.NIVEL_PROXIMIDAD,
-                resultado: this.#obtenerNivelProximidad(n)
-            }
+        if ( this.#hasAcertado ) {
+            return null
         }
+
+        if ( this.#adivinanza === n ) {
+            this.#hasAcertado = true
+        }
+
+        let resultado = {
+            hasAcertado: this.#hasAcertado,
+            tipoResultado: this.#tipoResultado,
+            valor: undefined
+        }
+
+        if ( this.tipoResultado === this.AFIRMACIÓN ) {
+            resultado.valor = this.#hasAcertado
+        } else if ( this.tipoResultado === this.COMPARACIÓN ) {
+            resultado.valor = this.#comparar(n)
+        } else if ( this.tipoResultado === this.NIVEL_PROXIMIDAD ) {
+            resultado.valor = this.#obtenerNivelProximidad(n)
+        }
+
+        this.#intentos++
+
+        return resultado
     }
 }
 
