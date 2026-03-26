ffa0347f80f18a8606a6b9d00e758379831a6399
yardas-dev <selector.yardas_0u@icloud.com>
2026-03-26 20:19:56 +0100

    Refactoriza la clase Juego para usar constantes y getters

--- juego.js
+++ juego.js
@@ -1,6 +1,9 @@
+TIPO_ES_MAYOR = 'es mayor (booleano)'
+TIPO_NIVEL_PROXIMIDAD = 'nivel de proximidad (decimal)'
+TIPOS_VÁLIDOS = [TIPO_ES_MAYOR, TIPO_NIVEL_PROXIMIDAD]
+ERROR_TIPO_INVÁLIDO = 'Tipo de pista inválido'
+
 class Juego {
-    TIPO_ES_MAYOR = 'es mayor (booleano)'
-    TIPO_NIVEL_PROXIMIDAD = 'nivel de proximidad (decimal)'
 
     #adivinanza = undefined
     #inicio_rango = undefined
@@ -9,11 +12,52 @@ class Juego {
     #intentos = []
     #tipoPista = undefined
 
-    constructor (tipoPista = this.TIPO_ES_MAYOR, inicio_rango = 1, fin_rango = 100) {
+    constructor (tipoPista = TIPO_ES_MAYOR, inicio_rango = 1, fin_rango = 100) {
+        if ( ! TIPOS_VÁLIDOS.includes(tipoPista) ) {
+            throw ERROR_TIPO_INVÁLIDO
+        }
+
         this.#adivinanza = Math.floor(Math.random() * fin_rango) + inicio_rango
         this.#inicio_rango = inicio_rango
         this.#fin_rango = fin_rango
         this.#tipoPista = tipoPista
+    }
+
+    get hasAcertado () {
+        return this.#hasAcertado
+    }
+
+    get intentos () {
+        return this.#intentos
+    }
+
+    get pista () {
+        let valor = undefined
+
+        if ( this.#hasAcertado ) {
+            valor = null
+        }
+
+        switch ( this.#tipoPista ) {
+            case this.TIPO_ES_MAYOR:
+                valor = this.#esMayor()
+
+            case this.TIPO_NIVEL_PROXIMIDAD:
+                valor = this.#obtenerNivelProximidad()
+
+            default:
+                valor = null
+        }
+
+        return {
+            intento: this.#intento,
+            tipo: this.#tipoPista,
+            valor
+        }
+    }
+
+    get #intento () {
+        return this.#intentos[this.#intentos.length - 1]
     }
 
     adivinar (n) {
@@ -28,39 +72,6 @@ class Juego {
         this.#intentos.push(n)
 
         return this.#hasAcertado
-    }
-
-    verIntento () {
-        return this.#intentos[this.#intentos.length - 1]
-    }
-
-    verIntentos () {
-        return this.#intentos
-    }
-
-    verPista () {
-        if ( this.#hasAcertado ) {
-            return null
-        }
-
-        switch ( this.#tipoPista ) {
-            case this.TIPO_ES_MAYOR:
-                return this.#esMayor()
-
-            case this.TIPO_NIVEL_PROXIMIDAD:
-                return this.#obtenerNivelProximidad()
-
-            default:
-                return null
-        }
-    }
-
-    verSiHasAcertado () {
-        return this.#hasAcertado
-    }
-
-    verTipoPista () {
-        return this.#tipoPista
     }
 
     #esMayor () {
